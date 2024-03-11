15d318eee2f0069423ce7bce170d4c29371d0fa7
samuel <samuel@gmail.com>
2024-03-11 08:26:40 +0530

    changed made accoding to the Pr

--- frontend/app/helpers/login.helper.ts
+++ frontend/app/helpers/login.helper.ts
@@ -1,34 +1,29 @@
-export const LoginValidation = (credentials: UserCredentials, setCredentials: React.Dispatch<React.SetStateAction<UserCredentials>>, loginFormValidationError: ValidationError, setLoginFormValidationError: React.Dispatch<React.SetStateAction<ValidationError>>) => {
-const regexPatternPassword = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
-const regexPatternUsername = /^[a-zA-Z0-9_]{3,20}$/
-const isUsernameValid = regexPatternUsername.test(credentials.username)
-const isPasswordValid = regexPatternPassword.test(credentials.password)
-const error = {}
-usernameValid(isUsernameValid, error)
-passwordValid(isPasswordValid, error)
-usernamePasswrodValid(isUsernameValid, isPasswordValid, setLoginFormValidationError)
-setLoginFormValidationError({...error})
+import { type UserCredentials, type ValidationError } from "@/app/login/page"
 
-return loginFormValidationError
-}
-const usernamePasswrodValid = (isUsernameValid: boolean, isPasswordValid: boolean, setLoginFormValidationError) =>{
-  if (isUsernameValid && isPasswordValid) {
-    setLoginFormValidationError({})
-  }
-}
+export const LoginValidation = (
+  credentials: UserCredentials,
+  setValidationError: React.Dispatch<React.SetStateAction<ValidationError>>
+): void => {
+  const regexPatternUsername = /^[a-zA-Z0-9_]{3,20}$/
+  const regexPatternPassword =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+  const isUsernameValid = regexPatternUsername.test(credentials.username)
+  const isPasswordValid = regexPatternPassword.test(credentials.password)
 
-const passwordValid = (isPasswordValid: boolean, error: {}):void =>{
-  if (!isPasswordValid) {
-    error.password = "Password must be at least 8 characters long and contain at least one letter, one digit, and one special character."
-  } else {
-    delete error.password
-  }
-}
-
-const usernameValid = (isUsernameValid: boolean, error: {}):void => {
-  if (!isUsernameValid) {
-    error.username = "Username must consist of alphanumeric characters between 3 and 20 characters long."
-  } else {
-    delete error.username
-  }
+  setValidationError(
+    (prevError: ValidationError): ValidationError => ({
+      ...prevError,
+      username: isUsernameValid
+        ? ""
+        : "Invalid username. Please ensure it consists of alphanumeric characters between 3 and 20 characters long.",
+    })
+  )
+  setValidationError(
+    (prevError: ValidationError): ValidationError => ({
+      ...prevError,
+      password: isPasswordValid
+        ? ""
+        : "Invalid password. Requirements:- At least 1 letter, digit, and special character- Minimum 8 characters",
+    })
+  )
 }
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,98 +1,152 @@
 "use client"
-import Typography from '@mui/material/Typography'
-import Grid from '@mui/material/Grid'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { useState } from 'react'
-import { LoginValidation } from '../helpers/login.helper'
+import React, { useState } from "react"
+import Typography from "@mui/material/Typography"
+import Grid from "@mui/material/Grid"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { LoginValidation } from "@/app/helpers/login.helper"
 
 const Login = (): React.ReactElement => {
-  interface UserCredentials {
-    username: string
-    password: string
-  }
-  const defaultCredential: UserCredentials = {
-    username: "",
-    password: ""
-  }
-  interface ValidationError {
-    username: string
-    password: string
-  }
-  const defaultValidationError: ValidationError = {
-    username: "",
-    password: ""
-  }
-  const [credentials, setCredentials] = useState<UserCredentials>(defaultCredential)
-  const [loginFormValidationError, setLoginFormValidationError] = useState<ValidationError>(defaultValidationError)
-  const [errorReceived, setErrorReceived] = useState<ValidationError>(defaultValidationError)
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  const [credentials, setCredentials] =
+    useState<UserCredentials>(defaultCredential)
+  const [validationError, setValidationError] = useState<ValidationError>(
+    defaultValidationError
+  )
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.target
     setCredentials({
       ...credentials,
-      [name]: value
+      [name]: value,
     })
   }
-  const onLogin = (event) => {
+  const onLogin = (event: React.MouseEvent<HTMLButtonElement>): void => {
     event.preventDefault()
-    const errorReceived = LoginValidation(credentials, setCredentials, loginFormValidationError, setLoginFormValidationError)
-    setErrorReceived(errorReceived)
+    LoginValidation(credentials, setValidationError)
   }
-
-  return (<>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <form onSubmit={onLogin}>
-            <Grid sx={{ maxWidth: 545, p: 5 }}>
-              <Grid item xs={6}>
-                <Grid container spacing={2}>
-                      <Grid item xs={4}>
-                        <img src="/images/orange.png" width={100} height={100} align="center" align="right"/>
-                          </Grid>
-                      <Grid item xs={8}>
-                        <ThemeProvider theme={theme}>
-                          <Typography variant="h2" gutterBottom sx={{ color: theme.palette.primary.main, marginBottom: 0, display: "flex", justifyContent: "left", alignItems: "center", height:"100%" }}>
-                            Orange
-                          </Typography>
-                        </ThemeProvider>
-                      </Grid>
-                  <Grid item xs={12}>
-                    <TextField type="text" name="username" id="filled-basic" label="Username" variant="filled" value={credentials.username} onChange={handleInputChange} sx={{ width: '100%' }}/>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <ThemeProvider theme={theme}>
-                      <Typography variant='h6' gutterBottom sx={{color: theme.palette.secondary.main, fontSize:"small"}}>
-                        {errorReceived.username}
-                      </Typography>
-                    </ThemeProvider>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField type="password" name="password" id="Password-basic" label="Password" variant="filled" value={credentials.password} onChange={handleInputChange} sx={{ width: '100%' }}/>
-                  </Grid>
-                  <Grid item xs={12}>
-                  <ThemeProvider theme={theme}>
-                      <Typography variant='h6' gutterBottom sx={{color: theme.palette.secondary.main, fontSize:"small"}}>
-                      {errorReceived.password}
-                      </Typography>
-                    </ThemeProvider>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Button type="submit" variant="contained" sx={{ width: '100%', backgroundColor: '#EE4700', color: 'white', padding:1 }} >LOGIN</Button>
-                  </Grid>
-                </Grid>
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Grid sx={{ maxWidth: 545, p: 5 }}>
+          <Grid item xs={6}>
+            <Grid container spacing={2}>
+              <Grid item xs={4} style={{ textAlign: "right" }}>
+                <img src="/images/orange.png" width={100} height={100} />
+              </Grid>
+              <Grid item xs={8}>
+                <ThemeProvider theme={theme}>
+                  <Typography
+                    variant="h2"
+                    gutterBottom
+                    sx={{
+                      color: theme.palette.primary.main,
+                      marginBottom: 0,
+                      display: "flex",
+                      justifyContent: "left",
+                      alignItems: "center",
+                      height: "100%",
+                    }}
+                  >
+                    Orange
+                  </Typography>
+                </ThemeProvider>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  type="text"
+                  name="username"
+                  id="filled-basic"
+                  label="Username"
+                  variant="filled"
+                  value={credentials.username}
+                  onChange={onInputChange}
+                  sx={{ width: "100%" }}
+                />
+              </Grid>
+              <ThemeProvider theme={theme}>
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  sx={{
+                    color: theme.palette.secondary.main,
+                    fontSize: "small",
+                    paddingLeft: "18px",
+                  }}
+                >
+                  {validationError.username}
+                </Typography>
+              </ThemeProvider>
+              <Grid item xs={12}>
+                <TextField
+                  type="password"
+                  name="password"
+                  id="Password-basic"
+                  label="Password"
+                  variant="filled"
+                  value={credentials.password}
+                  onChange={onInputChange}
+                  sx={{ width: "100%" }}
+                />
+              </Grid>
+              <ThemeProvider theme={theme}>
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  sx={{
+                    color: theme.palette.secondary.main,
+                    fontSize: "small",
+                    paddingLeft: "18px",
+                  }}
+                >
+                  {validationError.password}
+                </Typography>
+              </ThemeProvider>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  sx={{
+                    width: "100%",
+                    backgroundColor: "#EE4700",
+                    color: "white",
+                    padding: 1,
+                  }}
+                  onClick={onLogin}
+                >
+                  LOGIN
+                </Button>
               </Grid>
             </Grid>
           </Grid>
-        </form>
+        </Grid>
       </div>
     </>
   )
 }
 
-
-export default Login
-
+export interface UserCredentials {
+  username: string
+  password: string
+}
+const defaultCredential: UserCredentials = {
+  username: "",
+  password: "",
+}
+export interface ValidationError {
+  username: string
+  password: string
+}
+const defaultValidationError: ValidationError = {
+  username: "",
+  password: "",
+}
 const theme = createTheme({
   palette: {
     primary: {
@@ -105,3 +159,4 @@ const theme = createTheme({
     },
   },
 })
+export default Login
